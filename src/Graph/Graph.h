ecaab871ea67a3169b15ffbd5bfc235974fd5665
Saul Novel Gonzalez <saul.novel.gonzalez@gmail.com>
2022-01-16 00:02:41 +0100

    Prepare project for github

--- src/Graph/Graph.h
+++ src/Graph/Graph.h
@@ -3,6 +3,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <set>
+#include <vector>
 
 namespace Graph
 {
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <Graph/Graph.h>
+#include <HashTable/HashTable.h>
+#include <LinkedList/LinkedList.h>
+#include <LinkedList/DoubleLinkedList.h>
+#include <Queue/Queue.h>
+#include <Stack/Stack.h>
+#include <Tree/BinarySearchTree.h>
+
+int main()
+{
+    return 0;
+}
