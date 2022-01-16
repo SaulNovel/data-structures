c5ee7815e36d207cdbf1ac10bfd462f8f779e8b1
Saul Novel Gonzalez <saul.novel.gonzalez@gmail.com>
2022-01-16 13:48:12 +0100

    Add custom implementation for std::vector

---
+++ src/Array/CustomVector.h
@@ -0,0 +1,474 @@
+#include <cstddef>
+#include <algorithm>
+#include <iostream>
+#include <string.h>
+#include <memory.h>
+
+/* Imitates a std::vector constainer
+The C++ standard specifies that a legal (i.e., standard-conforming) 
+implementation of vector must satisfy the following performance requirements:
+
+    Operation	    Speed
+    vector()	    O(1)
+    vector(n, x)	O(n)
+    size()	        O(1)
+    v[i]	        O(1)
+    push_back(x)	O(1)
+    pop_back	    O(1)
+    insert	        O(size())
+    erase	        O(size())
+    front, back	    O(1)
+
+Rule of 5:
+ - Destructor
+ - Copy constructor
+ - Copy assignment operator
+ - Move constructor
+ - Move assignement operator
+
+*/
+
+
+
+template<typename Vector>
+class VectorIterator
+{
+public:
+    using pointer           = typename Vector::pointer;
+    using const_pointer     = typename Vector::const_pointer;
+    using reference         = typename Vector::reference;
+    using const_reference   = typename Vector::const_reference;
+
+public:
+    VectorIterator(pointer ptr)
+    : m_Ptr(ptr) {}
+
+    VectorIterator& operator++()
+    {
+        m_Ptr++;
+        return *this;
+    }
+
+    VectorIterator operator++(int32_t)
+    {
+        VectorIterator iterator = *this;
+        ++(*this);
+        return iterator;
+    }
+
+    VectorIterator& operator--()
+    {
+        m_Ptr--;
+        return *this;
+    }
+
+    VectorIterator operator--(int32_t)
+    {
+        VectorIterator iterator = *this;
+        --(*this);
+        return iterator;
+    }
+
+    reference operator[](int32_t index)
+    {
+        return *(m_Ptr + index);
+    }
+
+    pointer operator->()
+    {
+        return m_Ptr;
+    }
+
+    reference operator*()
+    {
+        return *m_Ptr;
+    }
+
+    const_pointer operator->() const
+    {
+        return m_Ptr;
+    }
+
+    const_reference operator*() const
+    {
+        std::cout << "const reference " << std::endl;
+        return *m_Ptr;
+    }
+
+    bool operator==(const VectorIterator& other) const
+    {
+        return m_Ptr == other.m_Ptr;
+    }
+
+    bool operator!=(const VectorIterator& other) const
+    {
+        return !(*this == other);
+    }
+
+private:
+    pointer m_Ptr;
+};
+
+template<typename T>
+class Vector {
+
+public:
+
+    /*---- Member types -----------------------------------------*/
+
+    using value_type        = T;
+    using reference         = T&;
+    using const_reference   = T const&;
+    using pointer           = T*;
+    using const_pointer     = const T*;
+
+    using iterator          = VectorIterator<Vector<T>>;
+    using const_iterator    = VectorIterator<Vector<const T>>;
+    using size_type         = std::size_t;
+
+    //iterator begin() { return iterator(m_Data); }
+    //const_iterator cbegin() const { std::cout << "const begin" << std::endl; return const_iterator(m_Data); }
+
+public:
+
+    /**
+    * Constructor
+    * Allocates space but does not call the constructor
+    * Useful if the type T has an expensive constructor
+    * We preallocate space without initializing it giving 
+    * room to grow and shrink the buffer without re-allocating.
+    * 
+    * @param start 
+    * @param end
+    */
+    Vector(const size_type& capacity = 0, const T& value = T())
+    : m_Capacity(capacity)
+    , m_Size(capacity)
+    , m_Data(static_cast<T*>(::operator new(capacity * sizeof(T))))
+    { 
+        if (capacity == 0) { return; }
+
+        fill_m_Data(0, capacity, value);
+    }
+
+    /**
+    * Destructor
+    * Because elements are constructed in-place using placement new.
+    * Then we must manually call the destructor on the elements.
+    */
+    ~Vector()
+    {
+        clear();
+        ::operator delete(m_Data);
+    }
+
+    /**
+    * Copy constructor
+    * Elements are constructed in-place using placement new.
+    */
+    Vector(const Vector& v)
+    : m_Capacity(v.m_Capacity)
+    , m_Size(v.m_Size)
+    , m_Data(static_cast<T*>(::operator new(v.m_Capacity * sizeof(T))))
+    {
+        for (size_t i = 0; i < v.m_Size; i++) {
+            // placement operator
+            new(&m_Data[i]) T(v.at(i));
+        }    
+    }
+
+    // Copy assignement operator
+    Vector& operator=(const Vector& v)
+    {
+        if (&v == this) {
+            return *this;
+        }
+
+        Vector tmp(v);
+        tmp.swap(*this);
+        return *this;
+    }
+
+    /**
+    * Move constructor
+    * The parameter passed is the source object and the state used to define the new object's state
+    * The source object is left in a valid (but can be undefined state, 
+    * for example no longer the owner of the internal buffer anymore).
+    * 
+    * @param v 
+    */
+    Vector(Vector &&v)
+    : m_Capacity(0)
+    , m_Size(0)
+    , m_Data(nullptr)
+    {        
+        v.swap(*this);
+    }
+
+    /**
+    * Move assignement operator
+    * The parameter passed is the source object and the state used to define the new object's state
+    * The source object is left in a valid (but can be undefined state, 
+    * for example no longer the owner of the internal buffer anymore).
+    * 
+    * @param v 
+    */
+    Vector& operator=(Vector &&v)
+    {
+        if (this == &v) {
+            return *this;
+        }
+        
+        v.swap(*this);
+        return *this;
+    }
+
+    /*---- Capacity ---------------------------------------------*/
+
+    size_type size() const { return m_Size; }
+
+    size_type capacity() const { return m_Capacity; }
+    
+    bool empty() { return m_Size == 0; }
+
+    /**
+    * Increase the capacity of the vector to a value that's greater than capacity().
+    * If capacity is greater than the current capacity(), new storage is allocated,
+    * otherwise the method does nothing.
+    * 
+    * @param capacity
+    */
+    void reserve(const size_type& capacity)
+    {
+        if (capacity <= m_Capacity) { return; }
+
+        reAlloc(capacity);
+    }
+
+    // copy previous elements
+    // add more elements at the end
+    void resize(const size_type& size, const T& value = T())
+    {
+        T* newBlock = (T*)::operator new(size * sizeof(T));
+
+        size_type length = (size > m_Size) ? m_Size : size;
+
+        for (size_t i = 0; i < length; i++) {
+            // placement operator
+            new(&newBlock[i]) T(m_Data[i]);
+        }
+
+        clear();
+        ::operator delete(m_Data);
+
+        m_Data = newBlock;
+        m_Capacity = size;
+        m_Size = size;
+
+        if (size <= length) { return; }
+
+        // initialize rest of the elements
+        fill_m_Data(length, size, value);
+    }
+
+    /*---- Element access ---------------------------------------*/
+
+    T* data() { return m_Data; }
+
+    // Non-Validated element access
+    T& operator[](size_type index) { return m_Data[index]; }
+
+    const T& operator[](size_type index) const { return m_Data[index]; }
+
+    // Validated element access
+    T& at(size_type index)
+    {
+        if (index < 0 || index >= m_Size)
+        {
+            throw std::out_of_range("index " + std::to_string(index) + " is out of range");
+        }
+        return m_Data[index];
+    }
+
+    const T& at(size_type index) const
+    {
+        if (index < 0 || index >= m_Size)
+        {
+            throw std::out_of_range("index " + std::to_string(index) + " is out of range");
+        }
+        return m_Data[index];
+    }
+
+    /*---- Modifiers --------------------------------------------*/
+
+    /**
+    * Adds an element to the end, increases size()
+    * Increases capacity() if needed
+    * 
+    * @param value
+    * 
+    */
+    void push_back(const T& value)
+    {
+        reaAlloc_if_required();
+        
+        new(&m_Data[m_Size++]) T(value);
+    }
+
+    void push_back(T&& value)
+    {
+        reaAlloc_if_required();
+
+        new(&m_Data[m_Size++]) T(std::move(value));
+    }
+
+    /**
+    * Constructs an element in-place at the end, increases size()
+    * Increases capacity() if needed
+    * 
+    * @param args
+    * 
+    */
+    template<typename... Args>
+    T& emplace_back(Args&& ... args)
+    {
+        reaAlloc_if_required();
+
+        // Construct the element in place: placement new
+        new(&m_Data[m_Size]) T(std::forward<Args>(args)...);
+        // m_Data[m_size] = T(std::forward<Args>(args)...);
+       
+        return m_Data[m_Size++];
+    }
+
+    /**
+    * Removes the last element, decreases size()
+    * Does not modify capacity(), calls to the element destructor
+    * 
+    */
+    void pop_back()
+    {
+        if (m_Size > 0) { m_Data[--m_Size].~T(); }
+    }
+
+
+    /**
+    * Erases all elements from the container.
+    * After this call, size() returns zero.
+    * 
+    */
+    void clear()
+    {
+        for (size_t i = 0; i < m_Size; i++) {
+            m_Data[i].~T();
+        }
+        m_Size = 0;
+    }
+
+    void swap(Vector &v)
+    {
+        using std::swap;
+        swap(m_Capacity, v.m_Capacity);
+        swap(m_Size, v.m_Size);
+        swap(m_Data, v.m_Data);
+    }
+
+    /*---- Iterators --------------------------------------------*/
+
+    iterator begin() { return iterator(m_Data); }
+    const_iterator cbegin() const { return const_iterator(m_Data); }
+
+    iterator end() { return iterator(m_Data + m_Size); }
+    const_iterator cend() const { return const_iterator(m_Data + m_Size); }
+
+
+private:
+    // Size of the array
+    size_type m_Capacity = 0;
+    // Number of elements in the array
+    size_type m_Size = 0;
+
+    T* m_Data = nullptr;
+
+    void reaAlloc_if_required()
+    {
+        if (m_Size >= m_Capacity) {
+            size_type newCapacity = (m_Capacity == 0) ? 1 : (m_Capacity * 2);
+            reAlloc(newCapacity);
+        }
+    }
+
+    /**
+    * Allocates a new block of memory
+    * Copies/moves old elements into new blocks
+    * Deletes old block
+    *
+    * @param start 
+    * @param end
+    */
+    void reAlloc(size_t newCapacity)
+    {
+
+        T* newBlock = (T*)::operator new(newCapacity * sizeof(T));
+
+        m_Size = (newCapacity < m_Size) ? newCapacity : m_Size;
+
+        for (size_t i = 0; i < m_Size; i++) {
+
+            // move operator does not work
+            //newBlock[i] = std::move(m_Data[i]);
+
+            // Move element's in place?
+            new(&newBlock[i]) T(std::move(m_Data[i]));
+        }
+
+        for (size_t i = 0; i < m_Size; i++) {
+            m_Data[i].~T();
+        }
+
+        //delete[] m_Data;
+        //::operator delete(m_Data, m_Capacity * sizeof(T));
+        ::operator delete(m_Data);
+        m_Data = newBlock;
+        m_Capacity = newCapacity;
+    }
+
+    /**
+    * Initializes POD elements to a value within a range.
+    *
+    * @param start 
+    * @param end
+    * @param value
+    */
+    template<class Q = T> typename std::enable_if<std::is_pod<Q>::value>::type     
+    fill_m_Data(size_t start, size_t end, const Q& value)
+    {
+        if (value == Q()) {
+            size_t size = end - start;
+            memset(m_Data + start, 0, size * sizeof(*m_Data));
+            return;
+        }
+
+        for (size_t i = start; i < end; i++) {
+            m_Data[i] = value;
+        }
+    }
+
+    /**
+    * Initializes non POD elements to a value within a range.
+    *
+    * @param start 
+    * @param end
+    * @param value
+    */
+    template<class Q = T> typename std::enable_if<!std::is_pod<Q>::value>::type 
+    fill_m_Data(size_t start, size_t end, const Q& value) 
+    {
+
+        for (size_t i = start; i < end; i++) {
+            // Placement operator
+            new(&m_Data[i]) T(value);
+        }        
+    }
+
+};
---
+++ test/CustomVectorTest.cpp
@@ -0,0 +1,523 @@
+#include <gtest/gtest.h>
+
+#include <Array/CustomVector.h>
+
+#include <string>
+#include <vector>
+#include <deque>
+
+using std::string;
+
+/* --- Scalar types --- */
+template <typename T>
+class VectorScalarTypesTest : public testing::Test {
+ public:
+  using VectorScalarTypes = Vector<T>;
+};
+
+template <>
+class VectorScalarTypesTest<std::string> : public testing::Test {
+ public:
+  using VectorScalarTypes = Vector<std::string>;
+};
+
+struct Point
+{
+    double x;
+    double y;
+
+    bool operator==(const Point& other) const
+    {
+        return (x == other.x) && (y == other.y);
+    }
+};
+
+std::ostream& operator<<(std::ostream& os, const Point& point)
+{
+    return os << "x: " << point.x << ", y: " << point.y;
+}
+
+/* Helper function to return a value depending on the parametrized type tested
+    - The current implementation requires dedicated templates for non POD types TODO: cast non POD to char*?
+*/
+template <typename T>
+const T getValue()
+{
+    return static_cast<T>(rand()) / static_cast<T>(RAND_MAX);
+}
+
+template <>
+const std::string getValue()
+{
+    const int32_t MAX = 26;
+    const int32_t strSize = 26;
+    char alphabet[MAX] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                          'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+                          'o', 'p', 'q', 'r', 's', 't', 'u',
+                          'v', 'w', 'x', 'y', 'z' };
+    std::string randomStr = "";
+    for (int32_t i = 0; i < strSize; i++) {
+        randomStr = randomStr + alphabet[rand() % strSize];
+    }      
+    return randomStr;
+}
+
+template <>
+const Point getValue()
+{
+    return {getValue<double>(), getValue<double>()};
+}
+
+template <typename T>
+const T getInitValue()
+{
+    return T();
+}
+
+using VectorScalarTypes = ::testing::Types<char, int32_t, double, Point, std::string>;
+
+TYPED_TEST_SUITE(VectorScalarTypesTest, VectorScalarTypes);
+
+/* --- Container types --- */
+
+template <typename T>
+class VectorNonScalarTypesTest : public testing::Test {
+ public:
+  using VectorNonScalarTypes = Vector<T>;
+};
+
+template <typename T>
+const T getNonScalarValue(size_t size)
+{
+    typename T::value_type value = static_cast<typename T::value_type>(rand()) / static_cast<typename T::value_type>(RAND_MAX);
+    return T(size, value);
+}
+
+// Dynamic Sequence Containers
+using VectorNonScalarTypes = ::testing::Types<std::vector<float>, std::deque<float>>;
+
+TYPED_TEST_SUITE(VectorNonScalarTypesTest, VectorNonScalarTypes);
+
+
+TYPED_TEST(VectorScalarTypesTest, Empty_constructor) 
+{
+    {
+        typename TestFixture::VectorScalarTypes temp;
+        EXPECT_EQ(0, temp.size());
+    }
+    {
+        typename TestFixture::VectorScalarTypes temp({});
+        EXPECT_EQ(0, temp.size());
+    }
+
+    {
+        size_t size = 20;
+        typename TestFixture::VectorScalarTypes temp(size);
+        EXPECT_EQ(size, temp.size());
+
+        for (size_t i = 0; i < temp.size(); ++i)
+        {
+            ASSERT_EQ(getInitValue<TypeParam>(), temp[i]) << " failed on index: " << i;
+        }
+    }
+}
+
+TYPED_TEST(VectorScalarTypesTest, Constructor_values)
+{
+    // TODO: update when iterators will be implmented
+    // ASSERT_THAT(std::vector<uint8_t>(buffer, buffer + buffer_size), ::testing::ElementsAreArray(expect));
+
+    size_t size = 20;
+    TypeParam value = getValue<TypeParam>();
+
+    typename TestFixture::VectorScalarTypes temp(size, value);
+    for (size_t i = 0; i < temp.size(); ++i)
+    {
+        ASSERT_EQ(value, temp[i]) << " failed on index: " << i;
+    }
+}
+
+TYPED_TEST(VectorScalarTypesTest, Move_constructor)
+{
+    size_t size = 20;
+    TypeParam value = getValue<TypeParam>();
+
+    typename TestFixture::VectorScalarTypes temp(size, value);
+    typename TestFixture::VectorScalarTypes temp2(std::move(temp));
+    
+    EXPECT_EQ(0, temp.size());
+    EXPECT_EQ(0, temp.capacity());
+    EXPECT_EQ(size, temp2.size());
+
+    for (size_t i = 0; i < temp2.size(); ++i)
+    {
+        ASSERT_EQ(value, temp2[i]) << " failed on index: " << i;
+    }
+}
+
+TYPED_TEST(VectorScalarTypesTest, Copy_constructor)
+{
+    size_t size = 20;
+    TypeParam value = getValue<TypeParam>();
+
+    typename TestFixture::VectorScalarTypes temp(size, value);
+    typename TestFixture::VectorScalarTypes temp2(temp);
+    
+    EXPECT_EQ(temp.size(), temp2.size());
+    EXPECT_EQ(temp.capacity(), temp2.capacity());
+
+    for (size_t i = 0; i < temp2.size(); ++i)
+    {
+        ASSERT_EQ(temp[i], temp2[i]) << " failed on index: " << i;
+    }
+}
+
+TYPED_TEST(VectorScalarTypesTest, Move_assignment)
+{
+    size_t size = 20;
+    TypeParam value = getValue<TypeParam>();
+
+    typename TestFixture::VectorScalarTypes temp(size, value);
+    typename TestFixture::VectorScalarTypes temp2;
+
+    temp2 = std::move(temp);
+
+    EXPECT_EQ(0, temp.size());
+    EXPECT_EQ(0, temp.capacity());
+    EXPECT_EQ(size, temp2.size());
+    for (size_t i = 0; i < temp2.size(); ++i)
+    {
+        ASSERT_EQ(value, temp2[i]) << " failed on index: " << i;
+    }
+
+    typename TestFixture::VectorScalarTypes temp3 = std::move(temp2);
+
+    EXPECT_EQ(0, temp2.size());
+    EXPECT_EQ(0, temp2.capacity());
+    EXPECT_EQ(size, temp3.size());
+    for (size_t i = 0; i < temp3.size(); ++i)
+    {
+        ASSERT_EQ(value, temp3[i]) << " failed on index: " << i;
+    }
+}
+
+TYPED_TEST(VectorScalarTypesTest, Copy_assignment)
+{
+    size_t size = 20;
+    TypeParam value = getValue<TypeParam>();
+
+    typename TestFixture::VectorScalarTypes temp(size, value);
+    typename TestFixture::VectorScalarTypes temp2;
+
+    temp2 = temp;
+
+    EXPECT_EQ(temp.capacity(), temp2.capacity());
+    EXPECT_EQ(temp.size(), temp2.size());
+    for (size_t i = 0; i < temp2.size(); ++i)
+    {
+        ASSERT_EQ(temp[i], temp2[i]) << " failed on index: " << i;
+    }
+
+    typename TestFixture::VectorScalarTypes temp3 = temp2;
+    
+    EXPECT_EQ(temp2.capacity(), temp3.capacity());
+    EXPECT_EQ(temp2.size(), temp3.size());
+    for (size_t i = 0; i < temp3.size(); ++i)
+    {
+        ASSERT_EQ(temp2[i], temp3[i]) << " failed on index: " << i;
+    }
+}
+
+TYPED_TEST(VectorScalarTypesTest, Reserve)
+{
+    size_t size = 20;
+    size_t capacity = 200;
+    
+    typename TestFixture::VectorScalarTypes temp(size);
+
+    temp.reserve(capacity);
+
+    EXPECT_EQ(size, temp.size());
+    EXPECT_EQ(capacity, temp.capacity());
+
+    // capacity < current capacity does nothing
+    temp.reserve(capacity-1);
+    EXPECT_EQ(capacity, temp.capacity());
+}
+
+TYPED_TEST(VectorScalarTypesTest, Resize)
+{
+    size_t size = 20;
+    size_t newSize = 200;
+    TypeParam value = getValue<TypeParam>();
+    TypeParam initValue = getInitValue<TypeParam>();
+    typename TestFixture::VectorScalarTypes temp(size, value);
+
+    {
+        temp.resize(newSize);    
+
+        ASSERT_EQ(newSize, temp.size());
+        for (size_t i = size; i < temp.size(); ++i) {
+            ASSERT_EQ(initValue, temp[i]) << " failed on index: " << i;
+        }
+    }
+
+    {
+        TypeParam value2 = getValue<TypeParam>();
+        newSize = 300;
+        size_t prevSize = temp.size();
+        temp.resize(newSize, value2);
+
+        ASSERT_EQ(newSize, temp.size());
+        for (size_t i = prevSize; i < temp.size(); ++i) {
+            ASSERT_EQ(value2, temp[i]) << " failed on index: " << i;
+        }
+
+    }
+
+    {
+        TypeParam value2 = getValue<TypeParam>();    
+        newSize = 100;
+        temp.resize(newSize, value2);
+
+        ASSERT_EQ(newSize, temp.size());
+        for (size_t i = 0; i < temp.size(); ++i) {
+            if (i < size) {
+                ASSERT_EQ(value, temp[i]) << " failed on index: " << i;
+                continue;
+            }
+            ASSERT_EQ(initValue, temp[i]) << " failed on index: " << i;        
+        }
+    }
+
+    {
+        TypeParam value2 = getValue<TypeParam>();    
+        newSize = 0;
+        temp.resize(newSize, value2);
+
+        ASSERT_EQ(newSize, temp.size());
+    }
+}
+
+TYPED_TEST(VectorScalarTypesTest, Element_access)
+{
+    size_t size = 20;
+    TypeParam value = getValue<TypeParam>();
+    typename TestFixture::VectorScalarTypes temp(size, value);
+    for (size_t i = 0; i < temp.size(); ++i) {
+        ASSERT_EQ(value, temp.at(i)) << " failed on index: " << i;
+    }
+    
+    TypeParam newValue = getValue<TypeParam>();
+    temp.at(0) = newValue; 
+    ASSERT_EQ(newValue, temp.at(0));
+
+    const typename TestFixture::VectorScalarTypes tempConst(size, value);
+    for (size_t i = 0; i < temp.size(); ++i) {
+        ASSERT_EQ(value, tempConst.at(i)) << " failed on index: " << i;
+    }
+
+    try {
+        TypeParam val = temp.at(size);
+        throw;
+    }
+    catch(std::out_of_range const & err) {
+        EXPECT_EQ(err.what(), std::string("index " + std::to_string(size) + " is out of range"));
+    }
+    catch(...) {
+        FAIL() << "Expected std::out_of_range";
+    }
+
+    try {
+        TypeParam val = tempConst.at(size);
+        throw;
+    }
+    catch(std::out_of_range const & err) {
+        EXPECT_EQ(err.what(), std::string("index " + std::to_string(size) + " is out of range"));
+    }
+    catch(...) {
+        FAIL() << "Expected std::out_of_range";
+    }
+}
+
+TYPED_TEST(VectorScalarTypesTest, Data)
+{
+    size_t size = 20;
+    TypeParam value = getValue<TypeParam>();
+    typename TestFixture::VectorScalarTypes temp(size, value);
+    TypeParam* data = temp.data();
+    for (size_t i = 0; i < temp.size(); ++i) {
+        ASSERT_EQ(value, data[i]) << " failed on index: " << i;
+    }
+    
+}
+
+TYPED_TEST(VectorScalarTypesTest, Push_back)
+{
+
+    size_t size = 20;
+    typename TestFixture::VectorScalarTypes temp(size);
+    TypeParam value = getValue<TypeParam>();
+
+    temp.push_back(value);
+    ASSERT_EQ(size+1, temp.size());
+    ASSERT_EQ(size*2, temp.capacity());
+    ASSERT_EQ(value, temp[size]);
+
+    TypeParam value2 = value;
+    temp.push_back(std::move(value));
+    ASSERT_EQ(size+2, temp.size());
+    ASSERT_EQ(value2, temp[size+1]);
+    if (!std::is_trivially_default_constructible<TypeParam>::value)
+    {
+        // TODO: it expects that the object is left on a valid state after std::move
+        ASSERT_EQ(getInitValue<TypeParam>(), value);
+    }
+}
+
+TYPED_TEST(VectorScalarTypesTest, Emplace_back)
+{
+
+    size_t size = 20;
+    typename TestFixture::VectorScalarTypes temp(size);
+    TypeParam value = getValue<TypeParam>();
+
+    TypeParam value2 = value;
+    temp.emplace_back(std::move(value));
+    ASSERT_EQ(size+1, temp.size());
+    ASSERT_EQ(size*2, temp.capacity());
+
+    ASSERT_EQ(value2, temp[size]);
+}
+
+
+TYPED_TEST(VectorScalarTypesTest, Pop_back)
+{
+    typename TestFixture::VectorScalarTypes temp;
+
+    TypeParam initValue = getInitValue<TypeParam>();
+    temp.push_back(initValue);
+    TypeParam value = getValue<TypeParam>();
+    temp.push_back(value);
+    temp.push_back(getValue<TypeParam>());
+
+    size_t size = temp.size();
+    size_t capacity = temp.capacity();
+
+    temp.pop_back();
+
+    ASSERT_EQ(--size, temp.size());
+    ASSERT_EQ(capacity, temp.capacity());
+    ASSERT_EQ(temp.at(size-1), value);
+
+    temp.pop_back();
+
+    ASSERT_EQ(--size, temp.size());
+    ASSERT_EQ(temp.at(size-1), initValue);
+    ASSERT_EQ(capacity, temp.capacity());
+
+    temp.pop_back();
+
+    ASSERT_EQ(0, temp.size());
+    ASSERT_EQ(capacity, temp.capacity());
+}
+
+TYPED_TEST(VectorScalarTypesTest, Clear)
+{
+    size_t size = 20;    
+    typename TestFixture::VectorScalarTypes temp(size);
+    size_t capacity = temp.capacity();
+
+    temp.clear();
+
+    EXPECT_EQ(capacity, temp.capacity());
+    EXPECT_EQ(0, temp.size());
+}
+
+TYPED_TEST(VectorScalarTypesTest, Range_Loop)
+{
+    size_t size = 20;
+    TypeParam initValue = getValue<TypeParam>();
+    typename TestFixture::VectorScalarTypes temp(size, initValue);
+
+    // Ranged loop
+    for (TypeParam& value : temp)
+    {
+        ASSERT_EQ(value, initValue);
+    }
+
+    // Foreach loop
+    std::for_each(temp.begin(), temp.end(), [initValue](TypeParam value) {
+         ASSERT_EQ(value, initValue);
+    });
+
+    std::for_each(temp.cbegin(), temp.cend(), [initValue](const TypeParam& value) {
+        ASSERT_EQ(value, initValue);
+    });
+}
+
+TYPED_TEST(VectorScalarTypesTest, Iterators)
+{
+    size_t size = 20;
+    TypeParam initValue = getValue<TypeParam>();
+    typename TestFixture::VectorScalarTypes temp(size, initValue);
+
+    // Loop with Normal iterator
+    using Iterator = typename TestFixture::VectorScalarTypes::iterator;
+    for (Iterator it = temp.begin(); it != temp.end(); ++it)
+    {
+        ASSERT_EQ(*it, initValue);
+        TypeParam newValue = getValue<TypeParam>();
+        *it = newValue;
+        ASSERT_EQ(*it, newValue);
+    }
+
+    // Loop with constant iterator
+    typename TestFixture::VectorScalarTypes temp2(size, initValue);
+    using const_Iterator = typename TestFixture::VectorScalarTypes::const_iterator;
+    for (const_Iterator it = temp2.cbegin(); it != temp2.cend(); ++it)
+    {
+        ASSERT_EQ(*it, initValue);
+    }
+}
+
+// TODO make it parametrized
+TYPED_TEST(VectorNonScalarTypesTest, Containers)
+{
+    size_t containerSize = 20;
+    TypeParam container = getNonScalarValue<TypeParam>(containerSize);
+    typename TypeParam::value_type initValue = container[0];
+        
+    size_t size = 20;
+    typename TestFixture::VectorNonScalarTypes temp(size, container);
+
+    for (const TypeParam& containerRef : temp)
+    {
+        for (const float value : containerRef)
+        {
+            ASSERT_EQ(value, initValue);
+        }
+    }
+}
+
+struct A
+{
+    double x;
+    A() 
+    {
+        throw std::runtime_error("constructor throws");
+    }
+};
+
+TEST(VectorTest, Constructor_throws)
+{
+    // Implement default constructor noexcept
+    Vector<A> temp;
+    //std::vector<A> temp(20);
+}
+
+int main(int ac, char* av[])
+{
+    testing::InitGoogleTest(&ac, av);
+    return RUN_ALL_TESTS();
+} 
